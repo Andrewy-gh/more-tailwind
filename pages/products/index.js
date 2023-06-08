515a8467525f2350f9f590578af94181ed865e23
andy <andy.yu617@gmail.com>
2023-06-08 20:18:58 +0000

    desktop and mobile sidebar

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,27 +1,14 @@
-import React from 'react';
+import React from "react";
+import DesktopSideBar from "../../components/sidebar/desktop";
+import MobileSideBar from "../../components/sidebar/mobile";
 
 export default function Products(props) {
   return (
     <>
       {/* media query change 10% to a bigger value */}
-      <div className="grid grid-cols-[minmax(150px,_25%)_1fr]">
-        <div className="h-screen text-blue-500 border border-blue-500">
-          <div className="text-3xl p-4 border-b border-blue-500 hover:bg-blue-500 hover:text-white">
-            DIV ABOVE
-          </div>
-          <div className="text-3xl p-4 border-b border-blue-500 hover:bg-blue-500 hover:text-white">
-            SHOP ALL
-          </div>
-          <div className="text-3xl p-4 border-b border-blue-500 hover:bg-blue-500 hover:text-white">
-            CATEGORY 1
-          </div>
-          <div className="text-3xl p-4 border-b border-blue-500 hover:bg-blue-500 hover:text-white">
-            CATEGORY 2
-          </div>
-          <div className="text-3xl p-4 border-b border-blue-500 hover:bg-blue-500 hover:text-white">
-            CATEGORY 3
-          </div>
-        </div>
+      <div className="grid lg:grid-cols-[minmax(150px,_25%)_1fr]">
+        <DesktopSideBar />
+        <MobileSideBar />
         <div>Main Content</div>
       </div>
     </>
