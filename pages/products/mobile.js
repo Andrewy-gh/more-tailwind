3ee60dc498fb3d0325ffd0e99a487a2fa704c5fb
Andy-git985 <andy.yu617@gmail.com>
2023-06-08 07:49:05 -0400

    mobile page hamburger menu

---
+++ pages/products/mobile.js
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import clsx from 'clsx';
+
+export default function Mobile() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prevMenuOpen) => !prevMenuOpen);
+  };
+
+  return (
+    <>
+      <header className="w-full h-16 bg-indigo-600 drop-shadow-lg">
+        <div className="container px-4 md:px-0 h-full mx-auto flex justify-between items-center">
+          {/* Logo Here */}
+          <a className="text-yellow-400 text-xl font-bold italic" href="#">
+            Text{' '}
+          </a>
+
+          {/* Menu links here */}
+          <ul
+            id="menu"
+            className={clsx(
+              'fixed top-0 right-0 px-10 py-16 bg-gray-800 z-50 md:relative md:flex md:p-0 md:bg-transparent md:flex-row md:space-x-6',
+              { hidden: !isMenuOpen, 'w-full h-screen': isMenuOpen }
+            )}
+          >
+            <li className="md:hidden z-90 fixed top-4 right-6">
+              <a
+                href="javascript:void(0)"
+                className="text-right text-white text-4xl"
+                onClick={toggleMenu}
+              >
+                &times;
+              </a>
+            </li>
+
+            <li>
+              <a
+                className="text-white opacity-70 hover:opacity-100 duration-300"
+                href="#"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white opacity-70 hover:opacity-100 duration-300"
+                href="#"
+              >
+                Something
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white opacity-70 hover:opacity-100 duration-300"
+                href="#"
+              >
+                Forums
+              </a>
+            </li>
+
+            <li>
+              <a
+                className="text-white opacity-70 hover:opacity-100 duration-300"
+                href="#"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-white opacity-70 hover:opacity-100 duration-300"
+                href="#"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          {/* This is used to open the menu on mobile devices */}
+          <div className="flex items-center md:hidden">
+            <button
+              className="text-white text-4xl font-bold opacity-70 hover:opacity-100 duration-300"
+              onClick={toggleMenu}
+            >
+              &#9776;
+            </button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <h1 className="mt-40 text-4xl text-center">Text</h1>
+        <h2 className="mt-10 text-xl text-center">Text </h2>
+      </main>
+    </>
+  );
+}
