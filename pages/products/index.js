e82201852715df6ec8da0913d42b997ac5104c70
andy <andy.yu617@gmail.com>
2023-06-09 14:16:15 +0000

    navbar component

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import DesktopSideBar from "../../components/sidebar/desktop";
 import MobileSideBar from "../../components/sidebar/mobile";
+import NavBar from "../../components/navbar/index";
 
 export default function Products(props) {
   return (
     <>
       {/* media query change 10% to a bigger value */}
+      <NavBar />
       <div className="grid lg:grid-cols-[minmax(150px,_25%)_1fr]">
         <DesktopSideBar />
         <MobileSideBar />
